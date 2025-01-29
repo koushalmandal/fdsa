fbafcaccf2bba2a586604041f6cd0a7e0fd058cb
koushalmandal <koushalmandalbca24@rvu.edu.in>
2025-01-29 11:52:54 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,10 @@
+a=[10,89,9,56,4,80,8]
+largest=a[0]
+smallest=a[0]
+for i in range(0  ,len(  za)):
+    if(largest<a[i]):
+        largest=a[i]
+    elif(smallest>a[i]):
+        smallest=a[i]
+print(largest)
+print(smallest)
